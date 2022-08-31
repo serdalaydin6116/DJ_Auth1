09498f6dc2a1ffe3c5ee27456fc4895d93720c26
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-31 22:52:25 +0300

    urls and views were synched

--- user_example/urls.py
+++ user_example/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from .views import (
     home,
-    special
+    special,
+    register, password_change
 )
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name="home"),
     path('special/', special, name="special"),
+    path('register/', register, name="register"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change")
 ]
--- user_example/views.py
+++ user_example/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'user_example/index.html')
 
+
+@login_required
 def special(request):
     return render(request, "user_example/special.html")
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    # form = UserCreationForm()
+    # if request.method=='POST':
+    #     form=UserCreationForm(request,POST)
+    if form.is_valid():
+        form.save()
+
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+
+        login(request, user)
+        return redirect('home')
+    else:
+        form=UserCreationForm()
+
+    context = {
+            'form': form
+    }
+    return render(request, "registration/register.html", context)
+
+def password_change(request):
+    if request.method == 'POST':
+        # We will use user change form this time
+        # Import it
+        form = UserChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = UserChangeForm()
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, "registration/password_change.html", context)
